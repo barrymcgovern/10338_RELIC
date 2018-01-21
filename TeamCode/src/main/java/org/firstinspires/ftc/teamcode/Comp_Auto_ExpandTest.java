e028473be97c20704f5262400641d0868fe5290f
Dark Matters FTC <denabots@gmail.com>
2018-01-21 16:05:04 -0500

    led lights

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp_Auto_ExpandTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp_Auto_ExpandTest.java
@@ -23,32 +23,35 @@ public class Comp_Auto_ExpandTest extends Competition_Hardware_Relic {
 
         waitForStart();
 
-
+        try{
         runtime.reset();
         //get claw out
-runtime.reset();
-while (runtime.seconds() < .5) {
-    stackmotor.setPower(-.5);
-}
-stackmotor.setPower(-.1);
-clawl.setPosition(clawLStart);
-clawr.setPosition(clawRStart);
-runtime.reset();
-while (runtime.seconds() < 1) {}
-runtime.reset();
-while (runtime.seconds() < 2) {
-    stackmotor.setPower(.05);
-}
-clawl.setPosition(clawLEnd);
-clawr.setPosition(clawREnd);
-runtime.reset();
-while (runtime.seconds() < 2) {
-    stackmotor.setPower(-.5);
-}
-stackmotor.setPower(-.1);
-
+        runtime.reset();
+        while (runtime.seconds() < .5) {
+          stackmotor.setPower(-.5);
+        }
+        stackmotor.setPower(-.1);
+        clawl.setPosition(clawLStart);
+        clawr.setPosition(clawRStart);
+        runtime.reset();
+        while (runtime.seconds() < 1) {}
+        runtime.reset();
+        while (runtime.seconds() < 2) {
+          stackmotor.setPower(.05);
+        }
+        clawl.setPosition(clawLEnd);
+        clawr.setPosition(clawREnd);
+        runtime.reset();
+        while (runtime.seconds() < 2) {
+          stackmotor.setPower(-.5);
+        }
+        stackmotor.setPower(-.1);
         drive_code(0,0,0);
+        }catch (Exception e){
+            telemetry.addData("run ERROR", e.toString());
+            telemetry.update();
 
+        }
 
 
 
